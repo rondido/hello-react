cc21ee951c1ad099987eb17b6d3bd4c028fd295f
Park <rppr001@naver.com>
2021-10-26 16:12:05 +0900

    react class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-import Myname from './Myname';
-import Counter from './Counter';
+import MyComponent from './MyComponent';
 
 class App extends Component {
   render() {
-    // const value = 1;
-    // const style = {
-    //   backgroundColor:'blue'
-    // };
+
     return (
       <div >
-
-        <Myname rol="리액트"/>
-        <Counter/>
-
-        {/* {
-          (() => {
-              if(value === 1)return(<div>하나</div>)
-              if(value === 2)return(<div>둘</div>)
-              if(value === 3)return(<div>셋</div>)
-          })()
-        } */}
+        <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>
       </div>
     );
   }
---
+++ src/MyComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ProTypes from 'prop-types';
+
+class MyComponent extends Component {
+    static defaultProps ={
+            name:'기본 이름'
+    };
+    static ProTypes = {
+        name:ProTypes.string,
+        favoriteNumber:ProTypes.number.isRequired
+    }
+    render() {
+        const {name,favoriteNumber,children} = this.props; //비구조화 할당
+        return (
+            <div>
+            안녕하세요 제 이름 {name}입니다. <br/>
+            children 값은 {children} 입니다.
+            <br/>
+            제가 좋아하는 숫자는 {favoriteNumber}입니다.
+        </div>
+        );
+    }
+}
+
+MyComponent.defaultProps={
+    name:'기본이름'
+};
+
+export default MyComponent;
